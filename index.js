6fa7d27c0a6cc1f82c0ff19e02274e6f51a07a61
VioPaige <85783108+VioPaige@users.noreply.github.com>
2024-07-15 13:06:57 +0200

    removed wrtc

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require(`http`)
 const fs = require(`fs`)
 const path = require(`path`)
 const { Server } = require(`socket.io`)
-const wrtc = require(`wrtc`)
 
 const { parseUrlFromRequest, parseCookiesFromRequest, parseRoomFromSocket, parseDataUriFromFile } = require(`./lib/parsing`)
 const ioRouter = require(`./routing/iorouter.js`)
@@ -29,21 +28,6 @@ io.engine.on("connection_error", console.log)
 console.log(`Listening for connections`)
 io.on(`connection`, (socket) => {
     ioRouter(socket)
-    
-    // socket.on(`disconnect`, () => {})
-
-    // let connection
-    // socket.on(`offer`, async (offer) => {
-    //     connection = new wrtc.RTCPeerConnection() // removed await here?
-
-    //     connection.onicecandidate = (e) => {
-    //         if (e.candidate) socket.emit(`iceCandidate`, JSON.stringify(e.candidate))
-    //     }
-
-
-    // })
-
-    // socket.on(`offer`, (offer) => )
 })
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
