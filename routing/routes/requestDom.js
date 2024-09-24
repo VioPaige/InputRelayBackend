f21d20d5fdaf1d93f23b55bb50f2c94d771e3924
VioPaige <85783108+VioPaige@users.noreply.github.com>
2024-09-24 22:25:29 +0200

    Update requestDom.js

--- routing/routes/requestDom.js
+++ routing/routes/requestDom.js
@@ -2,10 +2,12 @@ const connectionManager = require(`../../lib/connectionManager.js`)
 
 module.exports = {
     handler: (socket, data) => {
+        if (!data.key || !data.peerjscode) return
+
         let success = connectionManager.makeConnection(data.key, socket)
         socket.emit(`connectionStatus`, { success })
         
         if (!success) return
-        connectionManager.connections[data.key].controlled.emit(`domConnected`, { key: data.key, success })
+        connectionManager.connections[data.key].controlled.emit(`domConnected`, { key: data.key, peerjscode: data.peerjscode, success })
     }
 }
